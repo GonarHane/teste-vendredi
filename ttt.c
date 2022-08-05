a4438df4f80bb78eab487bbef459e7e46d681642
gonarhane <gonar90@gmail.com>
2022-08-05 08:37:36 +0000

    first commit

---
+++ ttt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,s;
+    
+    printf("entrer les deux nombre\n");
+    scanf("%d %d",&a,&b);
+    s=a+b;
+    printf("la somme est %d",s);
+    return 0;
+    
+
+}
